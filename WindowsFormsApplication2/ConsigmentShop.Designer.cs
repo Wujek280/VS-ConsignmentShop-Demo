e6ea8d33058c998edb2f84eab4f21993d0e70a75
Wujek280 <wujek5151@gmail.com>
2017-04-18 22:15:01 +0100

    init UI, Add Class Library init

---
+++ WindowsFormsApplication2/ConsigmentShop.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class ConsigmentShop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HeaderText = new System.Windows.Forms.Label();
+            this.itemListBox = new System.Windows.Forms.ListBox();
+            this.itemsListBoxLabel = new System.Windows.Forms.Label();
+            this.addToCart = new System.Windows.Forms.Button();
+            this.purchaseButton = new System.Windows.Forms.Button();
+            this.ShoppingCartListBoxLabel = new System.Windows.Forms.Label();
+            this.ShoppingCart = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // HeaderText
+            // 
+            this.HeaderText.AutoSize = true;
+            this.HeaderText.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.HeaderText.Location = new System.Drawing.Point(12, 9);
+            this.HeaderText.Name = "HeaderText";
+            this.HeaderText.Size = new System.Drawing.Size(575, 37);
+            this.HeaderText.TabIndex = 0;
+            this.HeaderText.Text = "Consignment Shop Demonstration App";
+            this.HeaderText.Click += new System.EventHandler(this.HeaderText_Click);
+            // 
+            // itemListBox
+            // 
+            this.itemListBox.FormattingEnabled = true;
+            this.itemListBox.ItemHeight = 20;
+            this.itemListBox.Location = new System.Drawing.Point(152, 86);
+            this.itemListBox.Name = "itemListBox";
+            this.itemListBox.Size = new System.Drawing.Size(261, 204);
+            this.itemListBox.TabIndex = 1;
+            // 
+            // itemsListBoxLabel
+            // 
+            this.itemsListBoxLabel.AutoSize = true;
+            this.itemsListBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.itemsListBoxLabel.Location = new System.Drawing.Point(148, 63);
+            this.itemsListBoxLabel.Name = "itemsListBoxLabel";
+            this.itemsListBoxLabel.Size = new System.Drawing.Size(96, 20);
+            this.itemsListBoxLabel.TabIndex = 2;
+            this.itemsListBoxLabel.Text = "List of Items";
+            this.itemsListBoxLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // addToCart
+            // 
+            this.addToCart.Location = new System.Drawing.Point(430, 159);
+            this.addToCart.Name = "addToCart";
+            this.addToCart.Size = new System.Drawing.Size(116, 43);
+            this.addToCart.TabIndex = 3;
+            this.addToCart.Text = "ADD ->";
+            this.addToCart.UseVisualStyleBackColor = true;
+            // 
+            // purchaseButton
+            // 
+            this.purchaseButton.Location = new System.Drawing.Point(742, 296);
+            this.purchaseButton.Name = "purchaseButton";
+            this.purchaseButton.Size = new System.Drawing.Size(82, 37);
+            this.purchaseButton.TabIndex = 6;
+            this.purchaseButton.Text = "BUY";
+            this.purchaseButton.UseVisualStyleBackColor = true;
+            // 
+            // ShoppingCartListBoxLabel
+            // 
+            this.ShoppingCartListBoxLabel.AutoSize = true;
+            this.ShoppingCartListBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShoppingCartListBoxLabel.Location = new System.Drawing.Point(559, 63);
+            this.ShoppingCartListBoxLabel.Name = "ShoppingCartListBoxLabel";
+            this.ShoppingCartListBoxLabel.Size = new System.Drawing.Size(77, 20);
+            this.ShoppingCartListBoxLabel.TabIndex = 5;
+            this.ShoppingCartListBoxLabel.Text = "Your Cart";
+            // 
+            // ShoppingCart
+            // 
+            this.ShoppingCart.FormattingEnabled = true;
+            this.ShoppingCart.ItemHeight = 20;
+            this.ShoppingCart.Location = new System.Drawing.Point(563, 86);
+            this.ShoppingCart.Name = "ShoppingCart";
+            this.ShoppingCart.Size = new System.Drawing.Size(261, 204);
+            this.ShoppingCart.TabIndex = 4;
+            // 
+            // ConsigmentShop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 357);
+            this.Controls.Add(this.purchaseButton);
+            this.Controls.Add(this.ShoppingCartListBoxLabel);
+            this.Controls.Add(this.ShoppingCart);
+            this.Controls.Add(this.addToCart);
+            this.Controls.Add(this.itemsListBoxLabel);
+            this.Controls.Add(this.itemListBox);
+            this.Controls.Add(this.HeaderText);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ConsigmentShop";
+            this.Text = "Consigment Shop v0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HeaderText;
+        private System.Windows.Forms.ListBox itemListBox;
+        private System.Windows.Forms.Label itemsListBoxLabel;
+        private System.Windows.Forms.Button addToCart;
+        private System.Windows.Forms.Button purchaseButton;
+        private System.Windows.Forms.Label ShoppingCartListBoxLabel;
+        private System.Windows.Forms.ListBox ShoppingCart;
+    }
+}
+
